18ab683b436e79805dc9eb7b75522db37b7e1bb8
1148648777@qq.com <1148648777@qq.com>
2023-04-04 21:40:18 +0800

    feat(send button): Add feature to enable/disable send button based on textarea input

--- static/index.js
+++ static/index.js
@@ -1,1 +1,11 @@
 console.log("Hello Panda Filehelper");
+
+function enableOrDisableSendButton(textarea) {
+    const sendButton = document.querySelector("div.msg-editor-send-button");
+
+    if (textarea.value !== "") {
+        sendButton.classList.add("msg-editor-send-button-enabled");
+    } else {
+        sendButton.classList.remove("msg-editor-send-button-enabled");
+    }
+}
